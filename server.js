b9b5308b68731e885426e13e79b747a0e9b71e94
gaurav.singh6 <gaurav.singh6@rsystems.com>
2020-01-14 19:01:05 +0530

    test for webhook

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.send('Hello World, i am new to this');
-=======
-  res.send('Hello World, I am new to this alien world');
->>>>>>> 16cb158a0a8a0b2d04af84ab14792bb7bfee6b03
+  res.send('Hello All, I am new to this alien world');
 });
 
 app.listen(PORT, HOST);
