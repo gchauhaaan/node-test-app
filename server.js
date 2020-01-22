6aba9cd12afd3f7d05da38a266a83836d71c35ee
gchauhaaan <gchauhaaan@gmail.com>
2020-01-22 15:10:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello All check, I am new to this alien world');
+  res.send('Hello All check, I am new to this world');
 });
 
 app.listen(PORT, HOST);
